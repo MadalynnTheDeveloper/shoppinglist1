bbdd4d59dff0c53cd6c5169969990d008d52b7a1
Madalynn Sandoval <sandovalmdalynn@gmail.com>
2020-09-28 21:30:21 -0600

    shoppinglist1

---
+++ script.js
@@ -0,0 +1,37 @@
+window.onload = function () {
+    initShoppingList();
+};
+
+function initShoppingList() {
+    let form = document.getElementById("item-form")
+
+    form.addEventListener("submit", (event) => {
+        handleItemForm(e, form);
+    });
+}
+
+function handleItemForm(event, formRef) {
+    if(e.preventDefault()) {
+        e.preventDefault();
+    }
+
+    addItemToShoppingList();
+
+    return false;
+}
+
+function addItemToShoppingList() {
+    let itemName = document.getElementById("item-name");
+    let itemAmount = document.getElementById("item-amount");
+
+    let itemHtml = createListItemHtml(itemName.value, itemAmount.value);
+    console.log("Item HTML:", itemHtml);
+    let itemListRef = document.getElementById("shopping-list");
+    itemListRef.insertAdjacentHTML("afterend", itemHtml) ;
+}
+
+function createListItemHtml(itemName, itemAmount) {
+    return '<li>
+                ${itemName} - ${itemAmount}
+                </li>';
+}
